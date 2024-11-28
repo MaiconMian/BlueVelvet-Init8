3114bbaf857063e7ecc475ce68d8e57cc52d6d0a
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2024-11-27 22:35:29 -0300

    Create a role DTO

--- src/main/java/com/bluevelvet/DTO/RoleDTO.java
+++ src/main/java/com/bluevelvet/DTO/RoleDTO.java
@@ -1,2 +1,22 @@
-package com.bluevelvet.DTO;public class RoleDTO {
+package com.bluevelvet.DTO;
+
+import com.bluevelvet.model.Permissions;
+import com.bluevelvet.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class RoleDTO {
+
+    @NotNull(message = "role name is required")
+    private String name;
+    private String description;
+    private List<Integer> permissions;
+
 }
