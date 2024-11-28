384c63e32b5a1d0de1802e67cdc8bc1dc5261821
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2024-11-27 22:33:19 -0300

    Adding services and roles for permission

--- src/main/java/com/bluevelvet/repository/PermissionsRepository.java
+++ src/main/java/com/bluevelvet/repository/PermissionsRepository.java
@@ -1,2 +1,9 @@
-package com.bluevelvet.repository;public class PermissionsRepository {
+package com.bluevelvet.repository;
+
+import com.bluevelvet.model.Permissions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionsRepository extends JpaRepository<Permissions, Integer> {
 }
