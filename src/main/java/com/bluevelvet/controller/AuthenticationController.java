369a9bb3be1fd0ba500aaaecb4bf101808894ff3
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2024-12-11 14:04:23 -0300

    Adding endpoint me for return users informations

--- src/main/java/com/bluevelvet/controller/AuthenticationController.java
+++ src/main/java/com/bluevelvet/controller/AuthenticationController.java
@@ -1,14 +1,21 @@
 package com.bluevelvet.controller;
 
 import com.bluevelvet.DTO.AdminRegisterDTO;
+import com.bluevelvet.DTO.UserInformationsDTO;
 import com.bluevelvet.DTO.LoginDTO;
 import com.bluevelvet.DTO.UserRegisterDTO;
+import com.bluevelvet.model.ApiResponse;
+import com.bluevelvet.model.Role;
 import com.bluevelvet.repository.UserRepository;
 import com.bluevelvet.security.TokenService;
 import com.bluevelvet.service.RoleService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
@@ -20,6 +27,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import com.bluevelvet.model.User;
 import com.bluevelvet.security.SecurityFilter;
+import io.jsonwebtoken.Jwts;
+import org.json.JSONObject;
+import java.security.SignatureException;
+import java.util.*;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwtParserBuilder;
+
+import static javax.crypto.Cipher.SECRET_KEY;
 
 @RestController
 @RequestMapping("auth")
@@ -35,6 +51,59 @@ public class AuthenticationController {
     private AuthenticationManager authenticationManager;
     @Autowired
     private SecurityFilter securityHelper;
+
+    @Value("${api.security.token.secret}")
+    private static String secret = "my-secret-key";
+
+    @GetMapping("/me")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<ApiResponse<Object>> getUserDetails(@RequestHeader(HttpHeaders.COOKIE) String cookieHeader){
+        try {
+
+            String token = extractTokenFromCookie(cookieHeader, "jwt");
+
+            if (token == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse<>("Token not found", null));
+            }
+
+            String email = decodeToken(token);
+            User user = (User) userRepository.findByEmail(email);
+            List<String> roleNames = new ArrayList<>();
+
+            for (Role role : user.getRoles()) {
+                roleNames.add(role.getName());
+            }
+
+            UserInformationsDTO userInformatiossDTO = new UserInformationsDTO(user.getName(), user.getLastName(), user.getEmail(), user.getImage(), roleNames);
+
+            return ResponseEntity.ok(new ApiResponse<>("User details fetched successfully", userInformatiossDTO));
+        } catch (JwtException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse<>(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(e.getMessage(), null));
+        }
+    }
+
+    private String decodeToken(String token){
+        String[] chunks = token.split("\\.");
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String payload = new String(decoder.decode(chunks[1]));
+        JSONObject json = new JSONObject(payload);
+        return json.getString("email");
+    }
+
+    private String extractTokenFromCookie(String cookieHeader, String cookieName) {
+        if (cookieHeader != null && !cookieHeader.isEmpty()) {
+            String[] cookies = cookieHeader.split(";");
+            for (String cookie : cookies) {
+                cookie = cookie.trim();
+                if (cookie.startsWith(cookieName + "=")) {
+                    return cookie.substring(cookieName.length() + 1);
+                }
+            }
+        }
+        return null;
+    }
 
     @PostMapping("/login")
     @PreAuthorize("permitAll()")
